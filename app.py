cd460156e64113c0508e05c11b2aed3915be2472
shahryar-cmyk <58168504+shahryar-cmyk@users.noreply.github.com>
2024-05-31 15:00:15 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,3 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def hello_world():
-#     return 'Hello World!'
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
-
 from flask import Flask, request, jsonify
 import subprocess
 
@@ -21,12 +10,16 @@ def execute_command():
 
     # Execute the command using subprocess
     try:
-        result = subprocess.check_output(command, shell=True, text=True)
+        result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
+        output = result.stdout
+        error = result.stderr
+        if error:
+            return jsonify({"error": error}), 500
     except subprocess.CalledProcessError as e:
         return jsonify({"error": f"Error executing command: {e.stderr}"}), 500
 
     # Return the result as a JSON response
-    return jsonify({"result": result})
+    return jsonify({"result": output})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
