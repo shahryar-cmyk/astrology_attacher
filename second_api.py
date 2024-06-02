1d0ff13f6b95d2361ea3af7c6a2c59e9557f33fd
shahryar-cmyk <58168504+shahryar-cmyk@users.noreply.github.com>
2024-06-02 18:00:39 +0500

    Update second_api.py

--- second_api.py
+++ second_api.py
@@ -7,5 +7,3 @@ def second_endpoint():
     # Define your second API logic here
     return jsonify({"message": "This is the second endpoint"})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
