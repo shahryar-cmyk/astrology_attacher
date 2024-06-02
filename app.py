1e0ab6789a74e00fbd65d212c42fcf0e584e95b6
shahryar-cmyk <58168504+shahryar-cmyk@users.noreply.github.com>
2024-06-02 14:36:49 +0500

    Update app.py

--- app.py
+++ app.py
@@ -49,8 +49,7 @@ def parse_swetest_output(output):
                 planet_name = match.group(1)
                 position = match.group(2).strip()
                 position1 = match.group(2).strip()
-                position = match.group(2).strip()
-
+                
 
 
                 # Extract the degree part of the position
@@ -60,7 +59,7 @@ def parse_swetest_output(output):
                 if degree_match:
                     degree = int(degree_match.group(1))
                     degree_sign = degree_match_sign..group(1)
-                    result[planet_name] = {"positionDegree": degree,"DegreeSign":degree_sign }
+                    result[planet_name] = {"positionDegree": degree,"DegreeSign":degree_sign}
                 else:
                     result[planet_name] = {"error": f"Error parsing degree from position: {position}"}
             else:
