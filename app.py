ffaa068c063623eec692521b6e8c275f246acc17
shahryar-cmyk <58168504+shahryar-cmyk@users.noreply.github.com>
2024-06-03 06:29:01 +0500

    Update app.py

--- app.py
+++ app.py
@@ -138,6 +138,7 @@ def parse_house_output(output):
                         "positionDegree": int(degree_match.group(1)) if degree_match else None,
                         "position_sign": degree_sign,
                         "position_min": minute,
+                        "position_sec": second,
 
                         
                     }
