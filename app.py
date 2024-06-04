c843fdcb58de7a43ed06c7436205493639680629
shahryar-cmyk <58168504+shahryar-cmyk@users.noreply.github.com>
2024-06-04 11:24:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -438,7 +438,7 @@ def parse_asteroid_output(asteroid_pholus_output):
             match1 = re.split(pattern1, lines[6])[1]
             degree_match1 = re.match(r"(\d{1,2})\s\w{2}\s.*", match1)
             degree_match_sign1 = re.findall(r'[a-zA-Z]+', match1)   
-            degree_sign1 = degree_match_sign1[0] if degree_match_sign1 else ""
+            degree_sign1 = degree_match_sign1[0] if match1 else ""
          
 
             result[name] = {
