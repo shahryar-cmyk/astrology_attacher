ba039e37f6148fb3d4a19bf2fb584798b8a16ce1
shahryar-cmyk <58168504+shahryar-cmyk@users.noreply.github.com>
2024-06-30 16:19:29 +0500

    Updating all planets data

--- apis/changeExcelGeneral.py
+++ apis/changeExcelGeneral.py
@@ -6,120 +6,228 @@ import pythoncom
 
 change_excel_general = Blueprint('change_excel_general', __name__)
 
+# Mapeo de las abreviaturas de los signos del zodíaco a sus nombres completos
 zodiac_signs = {
-    'ar': 'Aries', 'ta': 'Tauro', 'ge': 'Géminis', 'cn': 'Cáncer',
-    'le': 'Leo', 'vi': 'Virgo', 'li': 'Libra', 'sc': 'Escorpio',
-    'sa': 'Sagitario', 'cp': 'Capricornio', 'aq': 'Acuario', 'pi': 'Piscis'
+    'ar': 'Aries',
+    'ta': 'Tauro',
+    'ge': 'Géminis',
+    'cn': 'Cáncer',
+    'le': 'Leo',
+    'vi': 'Virgo',
+    'li': 'Libra',
+    'sc': 'Escorpio',
+    'sa': 'Sagitario',
+    'cp': 'Capricornio',
+    'aq': 'Acuario',
+    'pi': 'Piscis'
+    # Agrega otras abreviaturas si es necesario
 }
 
 @change_excel_general.route('/change_excel_general', methods=['POST'])
 def run_excel_macro_changeData():
-    pythoncom.CoInitialize()
+    pythoncom.CoInitialize()  # Initialize COM library
     try:
-        data = request.json
-        params = {
-            'birth_date_year': int(data.get('birth_date_year')),
-            'birth_date_month': int(data.get('birth_date_month')),
-            'birth_date_day': int(data.get('birth_date_day')),
-            'ut_hour': int(data.get('ut_hour')),
-            'ut_min': int(data.get('ut_min')),
-            'ut_sec': int(data.get('ut_sec')),
-            'lat_deg': data.get('lat_deg'),
-            'lon_deg': data.get('lon_deg')
-        }
+        # Get the parameters from the request data and ensure they are integers
+        birth_date_year = int(request.json.get('birth_date_year'))
+        birth_date_month = int(request.json.get('birth_date_month'))
+        birth_date_day = int(request.json.get('birth_date_day'))
+        ut_hour = int(request.json.get('ut_hour'))
+        ut_min = int(request.json.get('ut_min'))
+        ut_sec = int(request.json.get('ut_sec'))
+        lat_deg = request.json.get('lat_deg')
+        lon_deg = request.json.get('lon_deg')
 
-        commands = {
-            'houses': f"swetest -b{params['birth_date_day']:02d}.{params['birth_date_month']:02d}.{params['birth_date_year']} "
-                      f"-ut{params['ut_hour']:02d}:{params['ut_min']:02d}:{params['ut_sec']:02d} -p "
-                      f"-house{params['lat_deg']},{params['lon_deg']},P -fPZ -roundsec",
-            'planets': f"swetest -b{params['birth_date_day']:02d}.{params['birth_date_month']:02d}.{params['birth_date_year']} "
-                       f"-fPZ -ut{params['ut_hour']:02d}:{params['ut_min']:02d}:{params['ut_sec']:02d} -ep",
-            'quiron': f"swetest -ps -xs2060 -b{params['birth_date_day']:02d}.{params['birth_date_month']:02d}.{params['birth_date_year']} "
-                      f"-ut{params['ut_hour']:02d}:{params['ut_min']:02d}:{params['ut_sec']:02d} -fPZ -roundsec"
-        }
-
-        results = {key: subprocess.run(cmd, shell=True, check=True, capture_output=True, text=True).stdout.splitlines()
-                   for key, cmd in commands.items()}
-
-        def parse_output(output_lines, start, end, is_planet=False):
-            parsed_data = {}
-            pattern = r'\s{3,}'
-            for i, line in enumerate(output_lines[start:end], start=1):
-                try:
-                    match = re.split(pattern, line)[1]
-                    degree, sign, min_sec = re.match(r"(\d{1,2})\s(\w{2})\s(.*)", match).groups()
-                    sign = zodiac_signs.get(sign.lower(), sign)
-                    min, sec = re.split(r"[']", min_sec.replace(" ", ""))
-                    parsed_data[f"Casa{i}"] = {
-                        "positionDegree": int(degree),
-                        "position_sign": sign,
-                        "position_min": min,
-                        "position_sec": sec.replace('"', '')
-                    }
-                    if is_planet:
-                        parsed_data[f"Planet{i}"] = {"planet_name": line.split()[0], **parsed_data[f"Casa{i}"]}
-                except (IndexError, AttributeError):
-                    parsed_data[f"Casa{i}"] = {"error": "Error parsing line"}
-            return parsed_data
-
-        houses_data = parse_output(results['houses'], 8, 14)
-        planets_data = parse_output(results['planets'], 6, 16, is_planet=True)
-        quiron_data = parse_output(results['quiron'], 6, 7)
-
-        file_path = r'C:\El Camino que Creas\Generador de Informes\Generador de Informes\Generador de Informes.xlsm'
         xl = win32com.client.Dispatch("Excel.Application")
-        xl.Visible = False
-        wb = xl.Workbooks.Open(file_path)
-        sheet = wb.Sheets['CN y RS (o RL)']
+        xl.Visible = False  # Set to True if you want Excel to be visible
 
-        for key, details in houses_data.items():
-            if key.startswith("Casa"):
-                row = int(key[-1]) + 4
-                sheet.Range(f"E{row}").Value = details.get("positionDegree")
-                sheet.Range(f"D{row}").Value = details.get("position_sign")
-                sheet.Range(f"F{row}").Value = details.get("position_min")
-                sheet.Range(f"G{row}").Value = details.get("position_sec")
+        # Construct the command with zero-padded values
+        # For House Data From Cell D5 to D10
+        command = f"swetest -b{birth_date_day:02d}.{birth_date_month:02d}.{birth_date_year} -ut{ut_hour:02d}:{ut_min:02d}:{ut_sec:02d} -p -house{lat_deg},{lon_deg},P -fPZ -roundsec"
+        # For Planets Data From Cell D11 to D21 Which Includes True Node
+        command2 = f"swetest -b{birth_date_day:02d}.{birth_date_month:02d}.{birth_date_year} -fPZ -ut{ut_hour:02d}:{ut_min:02d}:{ut_sec:02d} -ep"
+        # For Quirón Command From Cell D22
+        quiron_planet = f"swetest -ps -xs2060 -b{birth_date_day:02d}.{birth_date_month:02d}.{birth_date_year} -ut{ut_hour:02d}:{ut_min:02d}:{ut_sec:02d} -fPZ -roundsec"
 
-        for idx, details in enumerate(planets_data.values(), start=1):
-            if "error" not in details:
-                row = idx + 28
-                sheet.Range(f"R{row}").Value = details.get("planet_name")
-                sheet.Range(f"U{row}").Value = details.get("positionDegree")
-                sheet.Range(f"S{row}").Value = details.get("position_sign")
-                sheet.Range(f"V{row}").Value = details.get("position_min")
-                sheet.Range(f"W{row}").Value = details.get("position_sec")
+        # Execute the command using subprocess
+        result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
+        result2 = subprocess.run(command2, shell=True, check=True, capture_output=True, text=True)
+        quiron_planet_result = subprocess.run(quiron_planet, shell=True, check=True, capture_output=True, text=True)
 
-        sheet.Range("R26").Value = quiron_data.get("name")
-        sheet.Range("S26").Value = quiron_data.get("positionDegree")
-        sheet.Range("T26").Value = quiron_data.get("position_sign")
-        sheet.Range("U26").Value = quiron_data.get("position_min")
+        output = result.stdout
+        lines = output.splitlines()
 
-        wb.Close(SaveChanges=True)
-        xl.Quit()
-        return jsonify({"message": "Data modified successfully.", "result": houses_data, "result2": planets_data, "asteriods": [quiron_data]}), 200
+        output2 = result2.stdout
+        lines2 = output2.splitlines()
 
+        quiron_output = quiron_planet_result.stdout
+        quiron_parse_output= parse_asteroid_output(quiron_output)
+
+        result_data = {}
+        planets = []
+        
+        # Parse the output for houses
+        if len(lines) > 0:
+            pattern = r'\s{3,}'  # Pattern to split by 3 or more spaces
+            for i in range(8, 14):  # Loop through lines 8 to 13 (houses 1 to 6)
+                try:
+                    match = re.split(pattern, lines[i])[1]
+                    degree_match = re.match(r"(\d{1,2})\s\w{2}\s.*", match)
+                    degree_match_sign = re.findall(r'[a-zA-Z]+', match)
+                    degree_sign_abbr = degree_match_sign[0] if degree_match_sign else ""
+                    degree_sign = zodiac_signs.get(degree_sign_abbr.lower(), degree_sign_abbr)
+                    degree_match_min_sec = re.sub(r'^.*?[a-zA-Z]', '', match)
+                    degree_match_min_sec_again = re.sub(r'^.*?[a-zA-Z]', '', degree_match_min_sec)
+                    degree_match_min_sec_again_spaces_removed = degree_match_min_sec_again.replace(" ", "")
+                    degree_match_min = degree_match_min_sec_again_spaces_removed.split("'")
+                    result_data[f"Casa{i - 7}"] = {
+                        "positionDegree": int(degree_match.group(1)) if degree_match else None,
+                        "position_sign": degree_sign,
+                        "position_min": degree_match_min[0],
+                        "position_sec": degree_match_min[1].replace('"', ''),  # Remove double quotes from seconds
+                    }
+                except IndexError as e:
+                    result_data["error"] = f"Error parsing output: {str(e)}"
+                    break
+        else:
+            result_data["error"] = "Error parsing line: No lines in the output"
+        
+        # Parse the output for planets
+        if len(lines2) > 0:
+            planet_positions = lines2[6:16]
+            planet_positions2 = lines2[17:18]
+
+            for line in planet_positions:
+                match = re.match(r"(\w+)\s+(.+)", line)
+                if match:
+                    planet_name = match.group(1)
+                    position = match.group(2).strip()
+                    degree_match = re.match(r"(\d{1,2})\s\w{2}\s.*", position)
+                    degree_match_sign = re.findall(r'[a-zA-Z]+', position)
+                    degree_sign_abbr = degree_match_sign[0] if degree_match_sign else ""
+                    degree_sign = zodiac_signs.get(degree_sign_abbr.lower(), degree_sign_abbr)
+                    degree_match_min_sec = re.sub(r'^.*?[a-zA-Z]', '', position)
+                    degree_match_min_sec_again = re.sub(r'^.*?[a-zA-Z]', '', degree_match_min_sec)
+                    degree_match_min_sec_again_spaces_removed = degree_match_min_sec_again.replace(" ", "")
+                    degree_match_min = degree_match_min_sec_again_spaces_removed.split("'")
+                    
+                    if degree_match:
+                        degree = int(degree_match.group(1))
+                        minute = degree_match_min[0]
+                        second = degree_match_min[1]
+                        
+                        planets.append({
+                            "planet_name": planet_name,
+                            "positionDegree": degree,
+                            "position_sign": degree_sign,
+                            "position_min": minute,
+                            "position_sec": second,
+                        })
+
+            for line in planet_positions2:
+                pattern = r"(True Node)\s+(\d+)\s+(\w+)\s+(\d+)\'([\d.]+)"
+                match = re.match(pattern, line, re.IGNORECASE)
+                if match:
+                    planets.append({
+                        "planet_name": match.group(1),
+                        "positionDegree": match.group(2),
+                        "position_sign": zodiac_signs.get(match.group(3), degree_sign_abbr),
+                        "position_min": match.group(4),
+                        "position_sec": match.group(5),
+                    })
+                else:
+                    planets.append({"error": f"Error parsing line: {line}"})
+        else:
+            planets.append({"error": "Error parsing output for planets: No lines in the output"})
+
+        # Open the workbook outside of the loop to avoid repeated opening and closing
+        try:
+            file_path = r'C:\El Camino que Creas\Generador de Informes\Generador de Informes\Generador de Informes.xlsm'
+            wb = xl.Workbooks.Open(file_path)  # Path to your Excel file
+            try:
+                sheet_name = 'CN y RS (o RL)'  # Replace with your sheet name
+                sheet = wb.Sheets(sheet_name)
+
+                # Modify data in the sheet based on the result_data
+                for casa, details in result_data.items():
+                    if casa.startswith("Casa"):
+                        degree_cell = f"E{int(casa[-1]) + 4}"  # Example cell for positionDegree
+                        sign_cell = f"D{int(casa[-1]) + 4}"  # Example cell for position_sign
+                        min_cell = f"F{int(casa[-1]) + 4}"  # Example cell for position_min
+                        sec_cell = f"G{int(casa[-1]) + 4}"  # Example cell for position_sec
+
+                        sheet.Range(degree_cell).Value = details["positionDegree"]
+                        sheet.Range(sign_cell).Value = details["position_sign"]
+                        sheet.Range(min_cell).Value = details["position_min"]
+                        sheet.Range(sec_cell).Value = details["position_sec"]
+
+                # Modify data in the sheet based on the planets
+                planet_row_start = 20  # Example starting row for planet data
+                for index, planet in enumerate(planets, start=1):
+                    if "error" not in planet:
+                        
+                        # sheet.Range(f"{index + 10}").Value = planet['planet_name']
+                        sheet.Range(f"E{index + 10}").Value = planet['positionDegree']
+                        sheet.Range(f"D{index + 10}").Value = planet['position_sign']
+                        sheet.Range(f"F{index + 10}").Value = planet['position_min']
+                        sheet.Range(f"G{index + 10}").Value = planet['position_sec']
+                    else:
+                        print(planet["error"])
+                
+                # sheet.Range("R26").Value = quiron_parse_output["name"]
+                # sheet.Range("S26").Value = quiron_parse_output["positionDegree"]
+                # sheet.Range("T26").Value = quiron_parse_output["position_sign"]
+                # sheet.Range("U26").Value = quiron_parse_output["position_min"]
+
+                print("Data modified successfully.")
+                return jsonify({"message": "Data modified successfully.", "result": result_data, "result2": planets, "asteriods": [quiron_parse_output] }), 200
+            finally:
+                wb.Close(SaveChanges=True)  # Save changes after running macro
+        except Exception as e:
+            print("Error opening workbook:", e)
+            return jsonify({"error": str(e)}), 500
+        finally:
+            xl.Quit()
     except Exception as e:
+        print("Error initializing Excel:", e)
         return jsonify({"error": str(e)}), 500
     finally:
-        pythoncom.CoUninitialize()
+        pythoncom.CoUninitialize()  # Uninitialize COM library
 
 def parse_asteroid_output(asteroid_pholus_output):
-    lines = asteroid_pholus_output.splitlines()
+    lines = asteroid_pholus_output.splitlines()  # Split by newline characters
     result = {}
+    
+    
     try:
-        pattern = r'\s{3,}'
-        match = re.split(pattern, lines[6])[1]
-        name = re.split(pattern, lines[6])[0]
-        degree, sign, min_sec = re.match(r"(\d{1,2})\s(\w{2})\s(.*)", match).groups()
-        sign = zodiac_signs.get(sign.lower(), sign)
-        min, sec = re.split(r"[']", min_sec.replace(" ", ""))
-        result[name] = {
-            "name": name,
-            "positionDegree": int(degree),
-            "position_sign": sign,
-            "position_min": min,
-            "position_sec": sec
-        }
-    except (IndexError, AttributeError):
-        result["error"] = "Error parsing output"
-    return result[name]
+        if len(lines) > 0:
+            pattern = r'\s{3,}'  # Pattern to split by 4 or more spaces
+            match = re.split(pattern, lines[6])[1]
+            name = re.split(pattern, lines[6])[0]
+            degree_match = re.match(r"(\d{1,2})\s\w{2}\s.*", match)
+            degree_match_sign = re.findall(r'[a-zA-Z]+', match)   
+            degree_sign = degree_match_sign[0] if degree_match_sign else ""
+            degree_match_min_sec = re.sub(r'^.*?[a-zA-Z]', '', match)
+            degree_match_min_sec_again = re.sub(r'^.*?[a-zA-Z]', '', degree_match_min_sec)
+            degree_match_min_sec_again_spaces_removed = degree_match_min_sec_again.replace(" ", "")
+            degree_match_min = degree_match_min_sec_again_spaces_removed.split("'")
+            # Only Teharonhiawako Left 
+            # When the degree is not found with the first pattern, try the second pattern
+            pattern1 = r'\s{2,}'  # Pattern to split by 3 or more spaces
+            match1 = re.split(pattern1, lines[6])[1]
+            degree_match1 = re.match(r"(\d{1,2})\s\w{2}\s.*", match1)
+
+            result[name] = {
+                      "name" : name,
+                    "positionDegree": int(degree_match.group(1)) if degree_match else degree_match1.group(1),
+                    "position_sign": degree_sign,
+                    "position_min": degree_match_min[0],
+                    "position_sec": degree_match_min[1] ,                    
+                
+    
+            }
+        else:
+            result["error"] = "Error parsing output: No lines in the output"
+    except IndexError as e:
+        result["error"] = f"Error parsing output: {str(e)}"
+
+    return result[name]  # Always return a dictionary
