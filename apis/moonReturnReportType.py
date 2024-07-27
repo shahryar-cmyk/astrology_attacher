509a8d3caf57765d1c8faf533d2a96a89ee4fabf
shahryar-cmyk <58168504+shahryar-cmyk@users.noreply.github.com>
2024-07-27 07:09:22 +0500

    Upto Min data is corrected

--- apis/moonReturnReportType.py
+++ apis/moonReturnReportType.py
@@ -1,14 +1,18 @@
 from flask import Blueprint, jsonify, request
 import swisseph as swe
 from datetime import datetime, timedelta
-import logging
+import subprocess
+import re
 
 # Initialize the Flask Blueprint
 solar_return_calculation = Blueprint('solar_return_calculation', __name__)
 
-# Configure logging
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
+# File path for output
+output_file_path = 'solar_return_calculation_output.txt'
+
+def write_to_file(content):
+    with open(output_file_path, 'a') as file:
+        file.write(content + '\n')
 
 @solar_return_calculation.route('/calculate_solar_return', methods=['POST'])
 def calculate_solar_return():
@@ -85,79 +89,109 @@ def calculate_solar_return():
                 elif diff < closest_dates[-1][0]:
                     closest_dates[-1] = (diff, positions)
                     closest_dates.sort(key=lambda x: x[0])
-                print(f"This is the difference: {diff}")
-
-                # Closest date found before minutes and second 
-        print(f"This is the befor date: {closest_dates}")
+                
         # Further refine the closest dates by minute and second
-        final_closest_dates = []
-        for closest_date in closest_dates:
-            base_date = datetime.strptime(closest_date[1]["date"], '%Y-%m-%d %H:%M:%S')
-            base_jd = julian_day(base_date.year, base_date.month, base_date.day,
-                                 base_date.hour, base_date.minute, base_date.second)
-            print(closest_dates)
-            print(f"This is the difference date: {base_date}")
-            print(f"This is the Closest date: {closest_dates[0][1]}")
-            # print(closest_dates)
-            # Extract differences and date dictionaries
-            # Extract differences and date dictionaries
-            # differences = [item[0] for item in base_date]
-            # date_dicts = [item[1] for item in base_date]
+        date_time_closest_date = datetime.strptime(closest_dates[0][1]["date"], '%Y-%m-%d %H:%M:%S')
+        
+        # Run the command for minutes from swetest
+        command_for_min = (f"swetest -b{date_time_closest_date.day}.{date_time_closest_date.month}.2024 "
+                           f"-p0 -fTPZ -n60 -s1m -ut{date_time_closest_date.hour}:{date_time_closest_date.minute}:{date_time_closest_date.second} -ep")
+        result_output_min = subprocess.run(command_for_min, shell=True, check=True, capture_output=True, text=True)
+        # write_to_file(f"result output for minutes: {result_output_min.stdout}")
+        # Separate the output by new line
+        result_output_min_newLine = result_output_min.stdout.split('\n')
+        # Get the sixth line of the output
+        most_closest_date_min = ''
+        # Loop through sixth and forward to get the closest date
+        for line in result_output_min_newLine[6:60]:
+            # Seperate the line by space 5 or more
+            # Split by spaces
+            splitbySpace = re.split(r'Sun', line)
+            
+            # Seperate the degree and minutes and seconds 
+            # Get the degree
+            degreeSplit = re.split(r'\s\s+', splitbySpace[1])
+            pattern = r'[a-zA-Z]'
+            splitByAlphaBets = re.split(pattern, degreeSplit[1])
+            # Degree
+            degreeMatch = splitByAlphaBets[0]
+            # Seperate by '
+            minSecMatch = re.split(r"'", splitByAlphaBets[2])
+            # Find the closest value of min given by the user
+            if int(minSecMatch[0]) == position_min:
+                # If the difference is less than 1 then write to the file
+                if abs(float(minSecMatch[1]) - position_sec) < 1:
 
-            # # Extract individual fields from date dictionaries
-            # dates = [d['date'] for d in date_dicts]
-            # sun_positions = [d['sun_position'] for d in date_dicts]
-            # print(f"This is the dates positions: {dates}")
+                    # write_to_file(f"Date: {splitbySpace[0]}  Degree: {degreeMatch} Minutes: {minSecMatch[0] } Seconds: {minSecMatch[1]}")
+                    # Remove UT from the date
+                    removedUTfromString = re.split(r'UT', splitbySpace[0])
+                    # remove the space from the string at last 
+                    removedUTfromString = removedUTfromString[0].rstrip()
+                    most_closest_date_min = datetime.strptime(f"{removedUTfromString}", "%d.%m.%Y %H:%M:%S").minute
+                    
 
-            for minute in range(60):
-                for second in range(60):
-                    jd = base_jd + (minute / 1440.0) + (second / 86400.0)
-                    transiting_sun_position, _ = swe.calc(jd, swe.SUN)
-                    date = base_date + timedelta(minutes=minute, seconds=second)
-                    positions = {
-                        "date": date.strftime('%Y-%m-%d %H:%M:%S'),
-                        "sun_position": transiting_sun_position[0]
-                    }
-                    # print(f"This is the positions: {base_date}")
-                    diff = abs(transiting_sun_position[0] - natal_sun_position)
-                    if len(final_closest_dates) < 2:
-                        final_closest_dates.append((diff, positions))
-                        final_closest_dates.sort(key=lambda x: x[0])
-                    elif diff < final_closest_dates[-1][0]:
-                        final_closest_dates[-1] = (diff, positions)
-                        final_closest_dates.sort(key=lambda x: x[0])
+        
 
-        # Get the most closest date
-        most_closest_date = final_closest_dates[0][1] if final_closest_dates else None
+        command_for_min = (f"swetest -b{date_time_closest_date.day}.{date_time_closest_date.month}.2024 "
+                           f"-p0 -fTPZ -n60 -s1s -ut{date_time_closest_date.hour}:{most_closest_date_min}:1 -ep")
+        result_output_sec = subprocess.run(command_for_min, shell=True, check=True, capture_output=True, text=True)
+        result_output_sec_newLine = result_output_sec.stdout.split('\n')
+        # Get the sixth line of the output
+        most_closest_date_sec = ''
+        for line in result_output_sec_newLine[6:60]:
+            # Seperate the line by space 5 or more
+            # Split by spaces
+            splitbySpace = re.split(r'Sun', line)
+            
+            # Seperate the degree and minutes and seconds 
+            # Get the degree
+            degreeSplit = re.split(r'\s\s+', splitbySpace[1])
+            pattern = r'[a-zA-Z]'
+            splitByAlphaBets = re.split(pattern, degreeSplit[1])
+            # Degree
+            degreeMatch = splitByAlphaBets[0]
+            # Seperate by '
+            minSecMatch = re.split(r"'", splitByAlphaBets[2])
+                # If the difference is less than 1 then write to the file
+            if abs(float(minSecMatch[1]) - position_sec) < 1:
 
-        if most_closest_date:
-            most_closest_datetime = datetime.strptime(most_closest_date["date"], '%Y-%m-%d %H:%M:%S')
-            most_closest_date_details = {
-                "year": most_closest_datetime.year,
-                "month": most_closest_datetime.month,
-                "day": most_closest_datetime.day,
-                "hour": most_closest_datetime.hour,
-                "minute": most_closest_datetime.minute,
-                "second": most_closest_datetime.second
-            }
-        else:
-            most_closest_date_details = None
+                    # write_to_file(f"Date: {splitbySpace[0]}  Degree: {degreeMatch} Minutes: {minSecMatch[0] } Seconds: {minSecMatch[1]}")
+                    # Remove UT from the date
+                    removedUTfromString = re.split(r'UT', splitbySpace[0])
+                    # remove the space from the string at last 
+                    removedUTfromString = removedUTfromString[0].rstrip()
+                    most_closest_date_sec = datetime.strptime(f"{removedUTfromString}", "%d.%m.%Y %H:%M:%S").minute
+
+            write_to_file(f"{result_output_min.stdout}")
+
+                    
+            
+
+
+            
+
+
+        
+
+        # Run the command for seconds from swetest
+        # command_for_sec = (f"swetest -b{date_time_closest_date.day}.{date_time_closest_date.month}.2024 "
+        #                    f"-p0 -fTPZ -n60 -s1s -ut{date_time_closest_date.hour}:{date_time_closest_date.minute}:{date_time_closest_date.second} -ep")
+        # result_output_sec = subprocess.run(command_for_sec, shell=True, check=True, capture_output=True, text=True)
+        # write_to_file(f"result output for seconds: {result_output_sec.stdout}")
 
         response = {
-            "closest_dates": [date[1] for date in final_closest_dates],
-            "most_closest_date": most_closest_date_details,
+            "closest_dates": closest_dates,
+            "most_closest_date": {
+                "date": closest_dates[0][1]["date"],
+                "hour": date_time_closest_date.hour,
+                "minute": most_closest_date_min
+            },
             "total": natal_sun_position
         }
 
         return jsonify(response), 200
 
     except Exception as e:
-        logger.error(f"Error occurred: {str(e)}")
+        # Write the error to the file
+        write_to_file(f"Error occurred: {str(e)}")
         return jsonify({"error": str(e)}), 500
-
-# Example usage:
-# from flask import Flask
-# app = Flask()
-# app.register_blueprint(solar_return_calculation, url_prefix='/api')
-# if __name__ == '__main__':
-#     app.run(debug=True)
