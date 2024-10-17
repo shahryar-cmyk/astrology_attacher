ddb2527a7451ca044e91bf329ee915db2c69ad0e
shahryar-cmyk <58168504+shahryar-cmyk@users.noreply.github.com>
2024-10-18 01:48:43 +0500

    Retrograde option fix
    This is the fixation of retrograde.

--- apis/calculate_moon_return.py
+++ apis/calculate_moon_return.py
@@ -532,7 +532,7 @@ def run_excel_macro_moon_change_data():
                         "position_sign": planet_true_node_parse_output.get('position_sign'),
                         "position_min": planet_true_node_parse_output.get('position_min'),
                         "position_sec": planet_true_node_parse_output.get('position_sec'),
-                        "retrograde": '' if planet_true_node_parse_output.get('retrograde') == 'R' else 'Shah'
+                        "retrograde": '' if planet_true_node_parse_output.get('retrograde') == 'R' else ''
                         }
         planet_true_node_parse_output.update(redrograde_logic_planet_true_node_parse_output)
         print(f"Data of the True Node: {planet_true_node_parse_output}")
@@ -1765,7 +1765,7 @@ def get_lunar_return_position_func(lat_deg,lon_deg,report_type_data,date):
                         "position_sign": planet_true_node_parse_output.get('position_sign'),
                         "position_min": planet_true_node_parse_output.get('position_min'),
                         "position_sec": planet_true_node_parse_output.get('position_sec'),
-                        "retrograde": '' if planet_true_node_parse_output.get('retrograde') == 'R' else 'Shah'
+                        "retrograde": '' if planet_true_node_parse_output.get('retrograde') == 'R' else ''
                         }
         planet_true_node_parse_output.update(redrograde_logic_planet_true_node_parse_output)
         # Hypothetical Planet 
